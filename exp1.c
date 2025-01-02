8758861ecfecba1cf056125b381859d202ed1f05
keerthanadurati <keerthanadurati71@gmail.com>
2025-01-02 00:37:46 -0500

    Add files via upload

---
+++ exp1.c
@@ -0,0 +1,13 @@
+#include<stdio.H>
+#include<conio.h>
+int main()
+{
+char str[15]={'h','e','l','l','o',',','w','o','r','l','d'};
+int i;
+printf("\N the actual string is %s",str);
+printf("\n the string after XOR each character with o is");
+for(i=0;i<11;i++)
+printf("%c",str[i]^o);
+return 0;
+}
+
